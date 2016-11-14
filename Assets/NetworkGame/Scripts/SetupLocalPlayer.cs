588ee3bf45ad337527470bfc7dcf0927d2555581
Arjun Prakash <archieoi@gmail.com>
2016-11-14 09:04:12 -0800

    Networking basics

---
+++ Assets/NetworkGame/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking; // added
+
+
+public class SetupLocalPlayer : NetworkBehaviour { // sun classed from network
+
+	string pname = "player";
+
+	void onGUI()
+	{
+		pname = GUI.TextField(new Rect (25, Screen.height - 40, 100, 30), pname);
+	}
+
+	// Use this for initialization
+	void Start () {
+		if(isLocalPlayer) // localPlayer from NetworkBehavior
+		{
+			GetComponent<DriverControl>().enabled = true;
+		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isLocalPlayer)
+		{
+			TextMesh[] tMesh = this.GetComponentsInChildren<TextMesh>();
+			tMesh[0].text = pname;
+
+		}
+	}
+}
